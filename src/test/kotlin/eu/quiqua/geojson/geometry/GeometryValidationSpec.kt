704aa9b2ff487ea8748ca89e4e9ab17977a8efb4
Marcel Radischat <marcel@door2door.io>
2018-08-20 16:59:13 +0200

    WIP add more tests and break them
    MockK's `spyk` currently can't handle the basically static methods
    defined in an object and is thus crashing :/

--- src/test/kotlin/eu/quiqua/geojson/geometry/GeometryValidationSpec.kt
+++ src/test/kotlin/eu/quiqua/geojson/geometry/GeometryValidationSpec.kt
@@ -2,6 +2,8 @@ package eu.quiqua.geojson.geometry
 
 import com.natpryce.hamkrest.assertion.assert
 import com.natpryce.hamkrest.isA
+import io.mockk.spyk
+import io.mockk.verify
 import org.jetbrains.spek.api.Spek
 import org.jetbrains.spek.api.dsl.context
 import org.jetbrains.spek.api.dsl.describe
@@ -9,25 +11,80 @@ import org.jetbrains.spek.api.dsl.it
 
 internal class GeometryValidationSpec : Spek({
     describe("Validate Point coordinates") {
-        context("valid input") {
-            it("returns ValidationResult.Ok for 2D coordinates") {
+        context("With valid input") {
+            it("Returns ValidationResult.Ok for 2D coordinates") {
                 val coordinates = Position(longitude = 1.0, latitude = 1.0)
                 assert.that(GeometryValidation.isPoint(coordinates), isA<ValidationResult.Ok>())
             }
-            it("returns ValidationResult.Ok for 3D coordinates") {
+            it("Returns ValidationResult.Ok for 3D coordinates") {
                 val coordinates = Position(longitude = 1.0, latitude = 1.0, altitude = 2.0)
                 assert.that(GeometryValidation.isPoint(coordinates), isA<ValidationResult.Ok>())
             }
         }
-        context("invalid input") {
-            it("returns ValidationResult.OutOfRangeError when longitude is out of range") {
+        context("With invalid input") {
+            it("Returns ValidationResult.OutOfRangeError when longitude is out of range") {
                 val coordinates = Position(longitude = 181.0, latitude = 1.0)
                 assert.that(GeometryValidation.isPoint(coordinates), isA<ValidationResult.OutOfRangeError>())
             }
-            it("returns ValidationResult.OutOfRangeError when latitude is out of range") {
+            it("Returns ValidationResult.OutOfRangeError when latitude is out of range") {
                 val coordinates = Position(longitude = 1.0, latitude = 91.0)
                 assert.that(GeometryValidation.isPoint(coordinates), isA<ValidationResult.OutOfRangeError>())
             }
         }
+    }
+    describe("Validate LineString coordinates") {
+        context("With at least two positions") {
+            it("Returns ValidationResult.Ok for 2D coordinates") {
+                val coordinates = listOf(
+                    Position(longitude = 1.0, latitude = 1.0),
+                    Position(longitude = 2.0, latitude = 2.0)
+                )
+                assert.that(GeometryValidation.isLineString(coordinates), isA<ValidationResult.Ok>())
+            }
+            it("Returns ValidationResult.Ok for 3D coordinates") {
+                val coordinates = listOf(
+                    Position(longitude = 1.0, latitude = 1.0, altitude = 1.0),
+                    Position(longitude = 2.0, latitude = 2.0, altitude = 2.0)
+                )
+                assert.that(GeometryValidation.isLineString(coordinates), isA<ValidationResult.Ok>())
+            }
+            it("Calls .isPoint for each coordinate in the linestring") {
+                val coordinates = listOf(
+                    Position(longitude = 1.0, latitude = 1.0),
+                    Position(longitude = 2.0, latitude = 2.0),
+                    Position(longitude = 3.0, latitude = 3.0)
+                )
+                val mock = spyk<GeometryValidation>()
+                mock.isLineString(coordinates)
+                verify(exactly = 3) { mock.isPoint(any()) }
+            }
+        }
+        context("With empty coordinates input") {
+            it("Returns ValidationResult.TooFewElements") {
+                val coordinates = emptyList<Position>()
+                assert.that(GeometryValidation.isLineString(coordinates), isA<ValidationResult.TooFewElements>())
+            }
+        }
+        context("With one coordinate input") {
+            it("Returns ValidationResult.TooFewElements") {
+                val coordinates = listOf(Position(longitude = 2.0, latitude = 2.0))
+                assert.that(GeometryValidation.isLineString(coordinates), isA<ValidationResult.TooFewElements>())
+            }
+        }
+        context("With different coordinate dimensions") {
+            it("Returns ValidationResult.IncompatibleCoordinateDimensions") {
+                val coordinates = listOf(
+                    Position(longitude = 2.0, latitude = 2.0),
+                    Position(longitude = 1.0, latitude = 1.0, altitude = 3.0)
+                )
+                assert.that(
+                    GeometryValidation.isLineString(coordinates),
+                    isA<ValidationResult.IncompatibleCoordinateDimensions>()
+                )
+            }
+        }
+    }
+    describe("Validate Polygon coordinates") {
+
     }
 })
