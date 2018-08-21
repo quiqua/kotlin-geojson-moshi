4baaf0ea8434d9ee3c40438080a136be3e5ebb48
Marcel Radischat <marcel@door2door.io>
2018-08-21 17:17:34 +0200

    Add tests for Polygon validation

--- src/test/kotlin/eu/quiqua/geojson/geometry/GeometryValidationSpec.kt
+++ src/test/kotlin/eu/quiqua/geojson/geometry/GeometryValidationSpec.kt
@@ -86,6 +86,89 @@ internal class GeometryValidationSpec : Spek({
         }
     }
     describe("Validate Polygon coordinates") {
-
+        context("With a linear ring") {
+            it("Returns ValidationResult.Ok for 2D coordinates") {
+                val coordinates = listOf(
+                    listOf(
+                        Position(longitude = 1.0, latitude = 1.0),
+                        Position(longitude = 2.0, latitude = 2.0),
+                        Position(longitude = 1.0, latitude = 3.0),
+                        Position(longitude = 1.0, latitude = 1.0)
+                    )
+                )
+                assert.that(GeometryValidation.isPolygon(coordinates), isA<ValidationResult.Ok>())
+            }
+            it("Returns ValidationResult.Ok for 3D coordinates") {
+                val coordinates = listOf(
+                    listOf(
+                        Position(longitude = 1.0, latitude = 1.0, altitude = 1.0),
+                        Position(longitude = 2.0, latitude = 2.0, altitude = 1.0),
+                        Position(longitude = 1.0, latitude = 3.0, altitude = 1.0),
+                        Position(longitude = 1.0, latitude = 1.0, altitude = 1.0)
+                    )
+                )
+                assert.that(GeometryValidation.isPolygon(coordinates), isA<ValidationResult.Ok>())
+            }
+            xit("Calls .isLineString for each list of coordinates") {
+                val coordinates = listOf(
+                    listOf(
+                        Position(longitude = 1.0, latitude = 1.0, altitude = 1.0),
+                        Position(longitude = 2.0, latitude = 2.0, altitude = 1.0),
+                        Position(longitude = 1.0, latitude = 3.0, altitude = 1.0),
+                        Position(longitude = 1.0, latitude = 1.0, altitude = 1.0)
+                    )
+                )
+                val mock = spyk<GeometryValidation>()
+                mock.isPolygon(coordinates)
+                verify(exactly = 1) { mock.isLineString(any()) }
+            }
+        }
+        context("With empty coordinates input") {
+            it("Returns ValidationResult.TooFewElements") {
+                val coordinates = emptyList<List<Position>>()
+                assert.that(GeometryValidation.isPolygon(coordinates), isA<ValidationResult.TooFewElements>())
+            }
+        }
+        context("With too few coordinates") {
+            it("Returns ValidationResult.NoLinearRing") {
+                val coordinates = listOf(
+                    listOf(
+                        Position(longitude = 1.0, latitude = 1.0),
+                        Position(longitude = 2.0, latitude = 2.0),
+                        Position(longitude = 1.0, latitude = 1.0)
+                    )
+                )
+                assert.that(GeometryValidation.isPolygon(coordinates), isA<ValidationResult.NoLinearRing>())
+            }
+        }
+        context("With different coordinates for start- and endpoint") {
+            it("Returns ValidationResult.NoLinearRing") {
+                val coordinates = listOf(
+                    listOf(
+                        Position(longitude = 1.0, latitude = 1.0),
+                        Position(longitude = 2.0, latitude = 2.0),
+                        Position(longitude = 3.0, latitude = 3.0),
+                        Position(longitude = 4.0, latitude = 4.0)
+                    )
+                )
+                assert.that(GeometryValidation.isPolygon(coordinates), isA<ValidationResult.NoLinearRing>())
+            }
+        }
+        context("With different coordinate dimensions") {
+            it("Returns ValidationResult.IncompatibleCoordinateDimensions") {
+                val coordinates = listOf(
+                    listOf(
+                        Position(longitude = 1.0, latitude = 1.0, altitude = 1.0),
+                        Position(longitude = 2.0, latitude = 2.0),
+                        Position(longitude = 3.0, latitude = 3.0),
+                        Position(longitude = 4.0, latitude = 4.0)
+                    )
+                )
+                assert.that(
+                    GeometryValidation.isPolygon(coordinates),
+                    isA<ValidationResult.IncompatibleCoordinateDimensions>()
+                )
+            }
+        }
     }
 })
